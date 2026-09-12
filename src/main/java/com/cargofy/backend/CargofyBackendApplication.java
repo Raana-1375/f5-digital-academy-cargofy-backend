a6a84cff5820ad1c26e0641a9a73eb06f5eec5f6
Raana <raanaafkari96@gmail.com>
2026-09-12 15:09:04 +0200

    feat: initialize Spring Boot project structure

---
+++ src/main/java/com/cargofy/backend/CargofyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cargofy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargofyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargofyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargofy/backend/CargofyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargofy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargofyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
